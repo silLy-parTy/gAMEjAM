749657f46182c3ca54a505b7d24aa69016fe6828
Luke Melrose <2201416@uad.ac.uk>
2023-02-04 12:31:36 +0000

    Added parallax Effect

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -25,12 +25,12 @@ public class ParallaxEffect : MonoBehaviour
         float temp = cam.transform.position.x * (1 - parallaxFactor);
         float distance = cam.transform.position.x * parallaxFactor;
 
-        Vector3 newPosition = new Vector3(startpos + distance, transform.position.y, transform.position.z- 10);
+        Vector3 newPosition = new Vector3(startpos + distance, transform.position.y, transform.position.z);
 
         transform.position = newPosition;
 
-        if (temp > startpos + (length / 2)) startpos += length;
-        else if (temp < startpos - (length / 2)) startpos -= length;
+        if (temp > startpos + (length)) startpos += length;
+        else if (temp < startpos - (length)) startpos -= length;
 
     }
 }
