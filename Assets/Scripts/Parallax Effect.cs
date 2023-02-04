df4fc15999d4c758e1e1b7061480c1038df6870a
Luke Melrose <2201416@uad.ac.uk>
2023-02-04 00:07:50 +0000

    Basic Parallax effect script set up

--- Assets/Scripts/Parallax Effect.cs
+++ Assets/Scripts/Parallax Effect.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ParallaxEffect : MonoBehaviour
 {
+
+    private float length, startpos;
+    public float parallaxFactor;
+    public GameObject cam;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float temp = cam.transform.position.x * (1 - parallaxFactor);
+        float distance = cam.transform.position.x * parallaxFactor;
+
+        Vector3 newPosition = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+
+        transform.position = newPosition;
+
+        if (temp > startpos + (length / 2)) startpos += length;
+        else if (temp < startpos - (length / 2)) startpos -= length;
+
     }
 }
