43c04151666f9884c183acebf4e7947c6e4d79f7
AlphAssassin <2201416@uad.ac.uk>
2023-02-05 02:38:57 +0000

    Redone Movement and collision and made opening to level

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,91 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Declare Variables
-   // public CharacterController2D controller;
-    public Rigidbody2D rb;
 
-    public Animator animator;
+    private Rigidbody2D rb;
 
-    public SpriteRenderer sR;
-    public Sprite standing;
-    public Sprite crouching;
-
-    public BoxCollider2D collide;
-    public CircleCollider2D circleCollide;
-
-    public Vector2 standingSize;
-    public Vector2 crouchingSize;
-
-
-
-    public float runSpeed;
-    public float jump;
-
-    public bool isJumping;
-    //private float nextJump = 0.5f;
-
-   // public bool jump = false;
-  //  public bool crouch = false;
-    float horizontalInput = 0f;
-
-    void Start()
+    // Start is called before the first frame update
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        collide = GetComponent<BoxCollider2D>();
-        collide.size = standingSize;
-
-        sR = GetComponent<SpriteRenderer>();
-        sR.sprite = standing;
-
-        standingSize = collide.size;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal") * runSpeed;
-        rb.velocity = new Vector2(runSpeed * horizontalInput, rb.velocity.y);
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        float dirX = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && isJumping == false)
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
-            animator.SetBool("IsJumping", true);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            sR.sprite = crouching;
-            collide.size = crouchingSize;
-            animator.SetBool("IsCrouching", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            sR.sprite = standing;
-            collide.size = standingSize;
-            animator.SetBool("IsCrouching", false);
-        }
- 
-    }
-
-    void OnCollisionEnter2D(Collision other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isJumping = false;
-            animator.SetBool("IsJumping", false);
-
+            rb.velocity = new Vector2(rb.velocity.x, 11f);
         }
     }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isJumping = true;
-        }
-    }
-
 }
